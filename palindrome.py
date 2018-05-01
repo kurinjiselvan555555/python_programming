13fc0c9fe5d07eb51cd671a253f32b7f786f6e62
kurinjiselvan555555 <37336688+kurinjiselvan555555@users.noreply.github.com>
2018-05-01 06:10:27 +0000

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,10 +2,10 @@ n=int(input())
 sum=0
 k=n
 while(n!=0):
-a=n%10
-sum=sum*10+a
-n=n/10
+	a=n%10
+	sum=sum*10+a
+	n=n/10
 if(sum==k):
-  print("palindrome")
+	print("palindrome")
 else:
   print("not palindrome")
